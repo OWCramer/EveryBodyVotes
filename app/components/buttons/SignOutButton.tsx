64d43c178a51c2105542b9f4485aa662e1d7aa73
Larvey <luisrvervaet@gmail.com>
2023-09-11 19:16:13 -0400

    oops

--- app/components/buttons/SignOutButton.tsx
+++ app/components/buttons/SignOutButton.tsx
@@ -2,8 +2,8 @@
 import { signOut } from "next-auth/react";
 import React from "react";
 
-const signOutButton = () => {
+const SignOutButton = () => {
   return <button onClick={() => signOut()}>Sign Out</button>;
 };
 
-export default signOutButton;
+export default SignOutButton;
