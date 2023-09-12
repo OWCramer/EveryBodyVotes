e7d122a5eb5f677016f5b467292103b84e9e0fe0
OWCramer <owencramer1218@gmail.com>
2023-09-11 22:04:27 -0400

    fix luis build error

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,26 @@ import SignInButton from "./components/buttons/SignInButton";
 
 import { getServerSession } from "next-auth/next";
 import type { NextRequest } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-export default async function Home(req: NextRequest): Promise<any> {
-  const session = await getServerSession(authOptions);
-  if (session) {
-    return (
-      <div>
-        <Navbar />
-        <h1>Signed in as {session?.user?.email}</h1>
-        <SignOutButton />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Navbar />
-        <h1>Not Signed In</h1>
-        <h1>Sign In Below</h1>
-        <SignInButton />
-      </div>
-    );
-  }
+export default async function Home(): Promise<any> {
+	const session = await getServerSession(options);
+	if (session) {
+		return (
+			<div>
+				<Navbar />
+				<h1>Signed in as {session?.user?.email}</h1>
+				<SignOutButton />
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<Navbar />
+				<h1>Not Signed In</h1>
+				<h1>Sign In Below</h1>
+				<SignInButton />
+			</div>
+		);
+	}
 }
